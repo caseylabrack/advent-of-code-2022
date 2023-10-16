c894c1a521a5a7cf9855fdade966c3a252d2c134
Casey Labrack <clabrack@gmail.com>
2023-10-16 03:28:39 -0400

    day 10 part 2 in Rust

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,14 +1,24 @@
 use std::fs;
+// use std::io::Write;
+// use std::io::stdout;
 
 fn main() {
 
+    let data = fs::read_to_string("data/input.txt").expect("couldn't read file"); 
+
+    let part1 = solve_part1(data.as_str());
+    println!("part 1: {part1}");
+
+    let part2 = solve_part2(data.as_str());
+    println!("part 2: \n{part2}");
+}
+
+fn solve_part1 (input: &str) -> i32 {
     let mut cycle: i32 = 1;
     let mut x = 1;
     let mut sum_of_signal_strengths = 0;
 
-    let example = fs::read_to_string("data/input.txt").expect("couldn't read file"); 
-
-    example
+    input
         .lines()
         .map(Instruction::from_string)
         .for_each(|op| {
@@ -38,8 +48,46 @@ fn main() {
 
         });
 
-        println!("final sum of signal strengths: {}", sum_of_signal_strengths);
+        sum_of_signal_strengths
+}
 
+fn solve_part2 (input: &str) -> String {
+
+    let mut message = String::new();
+    let mut cycle: i32 = 1;
+    let mut x = 2;
+
+    for instruction in input
+        .lines()
+        .map(Instruction::from_string) 
+    {
+        let ticks = match instruction {
+            Instruction::Addx(_z) => 2,
+            Instruction::Noop => 1
+        };
+
+        for _n in 0..ticks {
+
+            if cycle == 41 {
+                cycle = 1;
+                print!("\n");
+                message.push_str("\n");
+            }
+            
+            match i32::abs(x - cycle) <= 1 {
+                true => message.push_str("#"),
+                false => message.push_str(".")
+            };
+
+            cycle += 1;
+        }
+
+        x += match instruction {
+            Instruction::Addx(z) => z,
+            Instruction::Noop => 0
+        }
+    }
+    message
 }
 
 #[derive(Debug)]
